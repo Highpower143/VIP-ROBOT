570d2534dcc52ee4ef8ec9ca2fc95e1739a5f290
Highpower143 <nandyadu1c@gmail.com>
2025-01-15 19:37:22 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -20,7 +20,7 @@ class Config(object):
     LOGGER = True
     ALLOW_CHATS = True  # Yeh line add karna hoga
     DATABASE_URL = "mongodb+srv://yash:shivanshudeo@yk.6bvcjqp.mongodb.net/?retryWrites=true&w=majority&appName=yk"  # MongoDB URI here
-    # REQUIRED
+    START_IMG = "https://envs.sh/Nzg.jpg"# REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
     API_ID = "22180072" # integer value, dont use ""
