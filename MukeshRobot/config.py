5d9706de6b8e1276a83517a493c459d45a622499
Highpower143 <nandyadu1c@gmail.com>
2025-01-15 19:17:14 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -18,6 +18,7 @@ def get_user_list(config, key):
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
+    ALLOW_CHATS = True  # Yeh line add karna hoga
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
